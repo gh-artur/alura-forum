e8f8c737960cc542a17833254bb548da8e992303
Artur Santos <arturfrancisco86@gmail.com>
2022-10-04 14:26:53 -0300

    Projeto iniciado

---
+++ src/test/kotlin/com/ghartur/aluraforum/AluraForumApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ghartur.aluraforum
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AluraForumApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
